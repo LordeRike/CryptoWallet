cb656edf3e08825b5dae650f743f3b08122c50a7
Henrique <henrike.costaa@gmail.com>
2026-05-19 21:11:06 -0300

    add senha de acesso

--- Principal.java
+++ Principal.java
@@ -1,7 +1,8 @@
 public class Principal {
     public static void main(String[] args) {
-        Carteira minhCarteira = new Carteira("Henrique");
+        Carteira minhCarteira = new Carteira("Henrique", "1234");
         minhCarteira.depositar(0.5);
         System.out.println("Saldo de Bitcoin: "+minhCarteira.saldoBitcoin);
+        System.out.println("Senha de Acesso: "+minhCarteira.senhaAcesso);
     }
 } 
